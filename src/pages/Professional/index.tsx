936810e02df6f14a97b7b04fe3d4a8d20a971ec8
dan71853 <dan71853@gmail.com>
2026-07-06 13:17:36 +1200

    Added B200 images

--- src/pages/Professional/index.tsx
+++ src/pages/Professional/index.tsx
@@ -1,1 +1,1 @@
-export * as sosij from "./Test/Test";
+export * as B200 from "./B200/B200"
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,2 +1,2 @@
+export * as Professional from "./Professional/index"; 
 export * as Personal from "./Personal/index";
-export * as Professional from "./Professional/index";
