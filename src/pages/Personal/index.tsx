ebe09720e13921bef8da79c52ac3191bc36f1df8
dan71853 <dan71853@gmail.com>
2026-07-14 17:41:36 +1200

    feat: Added joystick page

--- src/pages/Personal/index.tsx
+++ src/pages/Personal/index.tsx
@@ -1,3 +1,4 @@
 export * as PowerDistribution from "./PowerDistributionBoard/PowerDistributionBoard";
+export * as Joystick from "./Joystick/Joystick"
 export const title = "Personal"
 export const priority = 1
