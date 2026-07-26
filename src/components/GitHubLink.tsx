fc374e200502cd23ed798e98a7e592b180dbe2ba
dan71853 <dan71853@gmail.com>
2026-07-26 12:06:53 +1200

    feat: Updated github component to better align

--- src/components/GitHubLink.tsx
+++ src/components/GitHubLink.tsx
@@ -4,16 +4,21 @@ import gitHubIconDark from "../assets/GithubIcon-Dark.svg";
 import gitHubIconLight from "../assets/GithubIcon-Light.svg";
 import type { gitHubIconProps } from "../types/types";
 
-export const GitHubLink = (props : gitHubIconProps) => {
+export const GitHubLink = (props: gitHubIconProps) => {
   return (
-    <Link to= {props.link} style={{ marginLeft: "auto" }}>
+    <Link to={props.link}>
       <img
         src={
           window.matchMedia("(prefers-color-scheme: dark)").matches
             ? gitHubIconDark
             : gitHubIconLight
         }
-        style={{ height: props.size??"80px" }}
+        style={{
+          height: props.size ?? "60px",
+          verticalAlign: "middle",
+          display: "inline-block",
+          marginLeft: "30px",
+        }}
       />
     </Link>
   );
