bc7eae8ba80d204eea0277643ccd358c0733e19a
dan71853 <dan71853@gmail.com>
2026-07-26 12:17:06 +1200

    feat: Updated remaining pages with github link

--- src/pages/3DPrints/Tallneck/Tallneck.tsx
+++ src/pages/3DPrints/Tallneck/Tallneck.tsx
@@ -2,22 +2,28 @@ import img from "./Thumbnail.jpg";
 
 import Img01 from "./Img-01.jpg";
 import Img02 from "./Img-02.jpg";
- 
+import { GitHubLink } from "../../../components/GitHubLink";
+
 export const page = () => {
   return (
     <>
-      <div style={{ textAlign: "left"}}>
-        <h1>Tallneck Robot</h1>
-            This is a robot from a game called Horizon Zero Dawn, I found the design on <a href="https://www.thingiverse.com/thing:2700455">Thingiverse</a>.  <br/>          
-            This was printed in multiple pats and glued together.
-        <figure >
-          <img src={Img01} width={600}/> 
-          <img src={Img02} width={600}/> 
+      <div style={{ textAlign: "left" }}>
+        <h1>
+          Tallneck Robot
+          <GitHubLink link="https://github.com/dan71853/MyProjects/tree/main/3DPrints/Tallneck" />
+        </h1>
+        This is a robot from a game called Horizon Zero Dawn, I found the design
+        on <a href="https://www.thingiverse.com/thing:2700455">Thingiverse</a>.{" "}
+        <br />
+        This was printed in multiple pats and glued together.
+        <figure>
+          <img src={Img01} width={600} />
+          <img src={Img02} width={600} />
           <figcaption>Pencil Holder</figcaption>
         </figure>
-
       </div>
-    </>);
+    </>
+  );
 };
 
 export const thumb = img;
